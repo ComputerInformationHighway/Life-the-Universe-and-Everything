7738371e613f51ff5dc27a581b0d9e3066db3ea1
Alex M <computerinformationhighway@gmail.com>
2015-10-30 00:31:28 -0400

    Final

---
+++ ComputerInformationHighway.java
@@ -0,0 +1,39 @@
+
+package computer.information.highway;
+
+import java.util.Random;
+
+public class ComputerInformationHighway {
+
+    public static void main(String[] args) {
+        
+        boolean answer;
+        
+        do {
+            
+        int newNumber = getNewNumber(1, 100);
+        answer = theAnswer(newNumber);
+        
+        System.out.println("What is the answer to life, the universe, and everything? Is it " + newNumber + "?");
+        
+        if(answer)
+            System.out.println("Yes!");
+        else
+            System.out.println("No!");
+        
+        }while(!answer);
+        
+    }
+    
+    public static boolean theAnswer(int supAns) {
+        return supAns == 42;
+    }
+    
+    public static int getNewNumber(int min, int max) {
+        
+        Random ran = new Random();
+        
+        return ran.nextInt(max - min + 1) + min;
+    }
+    
+}
